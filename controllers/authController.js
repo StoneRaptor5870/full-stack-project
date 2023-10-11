03dd1177935620a3b3b527de1b9bddf6ec01879f
Nischay <nischay.dtu@gmail.com>
2023-10-12 01:21:57 +0530

    auth: logout, protect, isLoggedIn

--- controllers/authController.js
+++ controllers/authController.js
@@ -65,3 +65,96 @@ export const login = catchAsync(async (req, res, next) => {
   // 3) If everything ok, send token to client
   createSendToken(user, 200, res);
 });
+
+export const logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    status: 'success',
+    message: 'Logged out',
+  });
+};
+
+export const protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check if its there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
+
+  if (!token) {
+    return next(
+      console.log(
+        'You are not authorized to access this! Please login to the application.',
+      ),
+    );
+  }
+
+  // 2) Verify token
+  const decoded = await promisify(jwt.verify)(
+    token,
+    process.env.JWT_SECRET_KEY,
+  );
+
+  // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      console.log('This user does not exist anymore! Please login again.'),
+    );
+  }
+
+  // 4) Check if use changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(console.log('Your password has changed! Please login again.'));
+  }
+
+  // 5) If everything ok
+  req.user = currentUser;
+  res.locals.user = currentUser;
+  next();
+});
+
+// Only for rendered pages, no errors!
+export const isLoggedIn = async (req, res, next) => {
+  if (req.cookies.jwt) {
+    try {
+      // 1) verify token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET_KEY,
+      );
+
+      // 2) Check if user still exists
+      const currentUser = await User.findById(decoded.id);
+      if (!currentUser) {
+        return next(
+          console.log('This user does not exist anymore! Please login again.'),
+        );
+      }
+
+      // 3) check if user changed password after the token was issued
+      if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next(
+          console.log('Your password has changed! Please login again.'),
+        );
+      }
+
+      // 4) There is a logged in user
+      res.locals.user = currentUser;
+      return next();
+    } catch (err) {
+      return next();
+    }
+  }
+  next();
+};
+
+export const restrictTo = (...roles) => {};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,15 @@
 import express from 'express';
-import { signup, login } from './../controllers/authController.js';
+import { signup, login, logout } from './../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
-// router.get('/logout', authController.logout);
-// router.post('/forgotPassword', authController.forgotPassword);
-// router.patch('resetPassword/:token', authController.resetPassword);
+router.get('/logout', logout);
+// router.post('/forgotPassword', forgotPassword);
+// router.patch('resetPassword/:token', resetPassword);
 
 // Protect all routes after this middleware
-// router.use(authController.protect);
+// router.use(protect);
 
 export default router;
