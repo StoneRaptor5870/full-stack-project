a2001ca4bec513a3d89a90198d58404f06c73964
Nischay <nischay.dtu@gmail.com>
2023-10-12 21:50:00 +0530

    error handling & auth: forgot password

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,8 @@ import { promisify } from 'util';
 import jwt from 'jsonwebtoken';
 import User from './../models/userModel.js';
 import catchAsync from './../utils/catchAsync.js';
+import AppError from '../utils/appError.js';
+import Email from '../utils/email.js';
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
@@ -52,14 +54,14 @@ export const login = catchAsync(async (req, res, next) => {
 
   // 1) Check if email and password exist
   if (!email || !password) {
-    return next(console.log('Please provide email and password'));
+    return next(new AppError('Please provide email and password!', 400));
   }
   // 2) Check if user exists && password is correct
   const user = await User.findOne({ email }).select('+password');
   //console.log(user);
 
   if (!user || !(await user.correctPassword(password, user.password))) {
-    return next(console.log('Invalid password or email'));
+    return next(new AppError('Incorrect email or password', 401));
   }
 
   // 3) If everything ok, send token to client
@@ -91,9 +93,7 @@ export const protect = catchAsync(async (req, res, next) => {
 
   if (!token) {
     return next(
-      console.log(
-        'You are not authorized to access this! Please login to the application.',
-      ),
+      new AppError('You are not logged in! Please log in to get access.', 401),
     );
   }
 
@@ -107,13 +107,18 @@ export const protect = catchAsync(async (req, res, next) => {
   const currentUser = await User.findById(decoded.id);
   if (!currentUser) {
     return next(
-      console.log('This user does not exist anymore! Please login again.'),
+      new AppError(
+        'The user belonging to this token does no longer exist.',
+        401,
+      ),
     );
   }
 
   // 4) Check if use changed password after the token was issued
   if (currentUser.changedPasswordAfter(decoded.iat)) {
-    return next(console.log('Your password has changed! Please login again.'));
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401),
+    );
   }
 
   // 5) If everything ok
@@ -135,16 +140,12 @@ export const isLoggedIn = async (req, res, next) => {
       // 2) Check if user still exists
       const currentUser = await User.findById(decoded.id);
       if (!currentUser) {
-        return next(
-          console.log('This user does not exist anymore! Please login again.'),
-        );
+        return next();
       }
 
       // 3) check if user changed password after the token was issued
       if (currentUser.changedPasswordAfter(decoded.iat)) {
-        return next(
-          console.log('Your password has changed! Please login again.'),
-        );
+        return next();
       }
 
       // 4) There is a logged in user
@@ -157,4 +158,48 @@ export const isLoggedIn = async (req, res, next) => {
   next();
 };
 
-export const restrictTo = (...roles) => {};
+export const restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403),
+      );
+    }
+
+    next();
+  };
+};
+
+export const forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on posted email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with email address.', 404));
+  }
+
+  // 2) Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  // 3) Send it to user's email
+  try {
+    const resetURL = `${req.protocol}://${req.get(
+      'host',
+    )}/api/v1/users/resetPassword/${resetToken}`;
+    await new Email(user, resetURL).sendPasswordReset();
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email!',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError('There was an error sending the email. Try again later!'),
+      500,
+    );
+  }
+});
