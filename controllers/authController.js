a9eada3ad8ddae1eee1dcab9308e32f78acaa90f
Nischay <nischay.dtu@gmail.com>
2023-10-12 22:18:17 +0530

    auth: reset & update password

--- controllers/authController.js
+++ controllers/authController.js
@@ -203,3 +203,57 @@ export const forgotPassword = catchAsync(async (req, res, next) => {
     );
   }
 });
+
+export const resetPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on reset token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  // 2) If token has not expired, and there is user, set the new password
+  if (!user) {
+    return next(
+      new AppError('Password reset token is invalid or has expired.', 400),
+    );
+  }
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  // 3) Update changedPasswordAt property for the user
+  // 4) Log the user in, send JWT
+  createSendToken(user, 200, res);
+});
+
+export const updatePassword = catchAsync(async (req, res, next) => {
+  // 1) Get user from the collection
+  const user = await User.findById(req.user.id).select('+password');
+
+  // 2) Check if current password is correct
+  if (!(await user.correctPassword(req.body.currentPassword, user.password))) {
+    return next(
+      new AppError(
+        'Your current password is incorrect. Please try again.',
+        401,
+      ),
+    );
+  }
+
+  // 3) If so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  // User.findByIdAndUpdate will NOT work as intended!
+
+  // 4) Log user in, send JWT
+  createSendToken(user, 200, res);
+});
